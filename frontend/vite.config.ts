f25582ef05b99f9a29d7adec38343a3de98de100
satyendra <satyendra@digihealth.in>
2025-11-04 11:44:32 +0530

    fix: manual resolutions of packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
@@ -17,10 +13,7 @@ export default defineConfig({
     port: Number(process.env.APP_PORT || 3045),
   },
   resolve: {
-    alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    },
+    alias: {},
   },
   optimizeDeps: {
     include: [
